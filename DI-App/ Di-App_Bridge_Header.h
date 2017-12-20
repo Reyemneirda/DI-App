ec298bfe550949370b44a93ae5b7f2e7582d609c
Gabriel Drai <Gabrieldrai@hotmail.fr>
2017-12-20 15:48:14 +0200

    Created Bridging file

---
+++ DI-App/ Di-App_Bridge_Header.h
@@ -0,0 +1,13 @@
+//
+//   Di-App_Bridge_Header.h
+//  DI-App
+//
+//  Created by Gabriel Drai on 20/12/2017.
+//  Copyright © 2017 Developer.Institute. All rights reserved.
+//
+
+#ifndef _Di_App_Bridge_Header_h
+#define _Di_App_Bridge_Header_h
+
+
+#endif /* _Di_App_Bridge_Header_h */
