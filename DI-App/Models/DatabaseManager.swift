badc98d2548d312bdd83f68c6b424f34bb3a07e2
Adrien Meyer <reyemneirda@gmail.com>
2017-12-28 11:44:48 +0200

    added the Datamanager Class

---
+++ DI-App/Models/DatabaseManager.swift
@@ -0,0 +1,10 @@
+//
+//  Database_Manager.swift
+//  DI-App
+//
+//  Created by Adrien Meyer on 28/12/2017.
+//  Copyright © 2017 Developer.Institute. All rights reserved.
+//
+
+import Foundation
+import Firebase
