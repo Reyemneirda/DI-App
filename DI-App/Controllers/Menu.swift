63327f9ff19beb197da83f102b45e8c4e0abcb43
Adrien Meyer <reyemneirda@gmail.com>
2018-01-10 11:57:09 +0200

    Optimized sideMenu

---
+++ DI-App/Controllers/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  DI-App
+//
+//  Created by Adrien Meyer on 10/01/2018.
+//  Copyright © 2018 Developer.Institute. All rights reserved.
+//
+
+import UIKit
+import SideMenu
+
+class Menu: UIStoryboardSegue {
+    
+
+    
+}
