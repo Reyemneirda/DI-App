c1f938430c3ecc8fc142983709ba2f53464368cf
Ethan <ethanahum@me.com>
2018-01-10 10:27:45 +0200

    Initial commit for branch ethanSearch

---
+++ DI-App/Controllers/New Group/CoursesTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CoursesTableViewCell.swift
+//  DI-App
+//
+//  Created by Administrateur on 08/01/2018.
+//  Copyright © 2018 Developer.Institute. All rights reserved.
+//
+
+import UIKit
+
+class CoursesTableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
