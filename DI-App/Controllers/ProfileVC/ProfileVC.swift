7fe5a4ddeacd1679fca8b5b7d24309496c09184f
Adrien Meyer <reyemneirda@gmail.com>
2018-01-15 15:19:30 +0200

    Set the picker, the tap recognizer and the relevant actions related to it, properly

--- DI-App/Controllers/ProfileVC/ProfileVC.swift
+++ DI-App/Controllers/ProfileVC/ProfileVC.swift
@@ -14,18 +14,20 @@ import FirebaseStorage
 
 
 class ProfileVC: BaseViewController, UIActionSheetDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     let storageRef = Storage.storage().reference(forURL: "gs://di-app-14896.appspot.com/")
     
-
+    
+    @IBOutlet var tapTheImage: UITapGestureRecognizer!
     @IBOutlet weak var fullNameTxt: UITextView!
     @IBOutlet weak var phoneNumberTxt: UITextView!
     @IBOutlet weak var emailTxt: UITextView!
     @IBOutlet weak var linkedinTxt: UITextView!
     @IBOutlet weak var sessionTxt: UITextView!
     
-
+    
     @IBOutlet weak var profilePic: UIImageView!
+    
     
     
     
@@ -34,6 +36,7 @@ class ProfileVC: BaseViewController, UIActionSheetDelegate, UIImagePickerControl
     @IBAction func takePhoto(_ sender: Any?) {
         let picker : UIImagePickerController = UIImagePickerController()
         picker.delegate = self
+        picker.allowsEditing = true
         picker.sourceType = .camera
         self.present(picker, animated: true, completion: nil)
     }
@@ -41,7 +44,9 @@ class ProfileVC: BaseViewController, UIActionSheetDelegate, UIImagePickerControl
     @IBAction func getPhotoFromLibrary(_ sender: Any?) {
         
         let picker : UIImagePickerController = UIImagePickerController()
-        picker.delegate = self 
+        picker.delegate = self
+        
+        picker.allowsEditing = true
         picker.sourceType = .photoLibrary
         
         self.present(picker, animated: true, completion: nil)
@@ -50,46 +55,20 @@ class ProfileVC: BaseViewController, UIActionSheetDelegate, UIImagePickerControl
     func imagePickerController (_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
         guard let selectedImage = info [UIImagePickerControllerOriginalImage] as? UIImage else { return }
         self.profilePic.image = selectedImage
-        
-        guard let userId = Auth.auth().currentUser?.uid else {return}
-        let uid = Auth.auth().currentUser?.uid
-        let key = Database.database().reference().child("students").childByAutoId().key
-        if let uploadData = UIImagePNGRepresentation(self.profilePic.image!)
-        {
-            DispatchQueue.main.async {
-
-                self.storageRef.putData(uploadData, metadata: nil, completion: { (metadata, error) in
-                if error != nil
-                {
-                    print(error as Any)
-                    
-                    return
-                }
-                let imageURL = metadata?.downloadURL()
-                let childUpdate = ["/profilPic/\(key)":imageURL]
-                 Database.database().reference().updateChildValues(childUpdate)
-
-                print(metadata)
-
-
-
-            })
-            }
-        }
-
         picker.dismiss(animated: true, completion: nil)
     }
     
     
-    @IBAction func getActionSheet(_ sender: Any) {
+    
+    @IBAction func tapHere(_ sender: Any) {
         let actionSheet = UIAlertController(title: "Profile Picture", message: "Pick an option", preferredStyle: .actionSheet)
         
         let cameraOpt = UIAlertAction(title: "Camera", style: .default, handler: {
             (action) in
             
             self.takePhoto(nil)
-          
-
+            
+            
         })
         
         actionSheet.addAction(cameraOpt)
@@ -109,13 +88,14 @@ class ProfileVC: BaseViewController, UIActionSheetDelegate, UIImagePickerControl
     }
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.profilePic.layer.cornerRadius = self.profilePic.frame.size.width / 2
-        self.actionSheetButton.layer.cornerRadius = self.actionSheetButton.frame.size.width / 2
+        tapTheImage.delegate = self
         
+        self.profilePic.isUserInteractionEnabled = true
+        self.profilePic.addGestureRecognizer(tapTheImage)
         displayProfile()
         
         
@@ -127,7 +107,7 @@ class ProfileVC: BaseViewController, UIActionSheetDelegate, UIImagePickerControl
     {
         let uid = Auth.auth().currentUser?.uid
         Database.database().reference().child("students").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
-          if let dict = snapshot.value as? [String:AnyObject]
+            if let dict = snapshot.value as? [String:AnyObject]
             {
                 self.fullNameTxt.text = dict["name"] as! String
                 self.emailTxt.text = dict["email"] as! String
@@ -137,18 +117,45 @@ class ProfileVC: BaseViewController, UIActionSheetDelegate, UIImagePickerControl
                 
             }
         }, withCancel: nil)
-            
+        
     }
     
- 
     
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func uploadPic()
+    {
+        let picker = UIImagePickerController()
+        
+        
+        
     }
     
+//    XXX
+//    guard let userId = Auth.auth().currentUser?.uid else {return}
+//    let uid = Auth.auth().currentUser?.uid
+//    let key = Database.database().reference().child("students").childByAutoId().key
+//    if let uploadData = UIImagePNGRepresentation(self.profilePic.image!)
+//    {
+//        DispatchQueue.main.async {
+//
+//            self.storageRef.putData(uploadData, metadata: nil, completion: { (metadata, error) in
+//                if error != nil
+//                {
+//                    print(error as Any)
+//
+//                    return
+//                }
+//                let imageURL = metadata?.downloadURL()
+//                let childUpdate = ["/profilPic/\(key)":imageURL]
+//                Database.database().reference().updateChildValues(childUpdate)
+//
+//                print(metadata)
+//
+//
+//
+//            })
+//        }
+//    }
+//    XXX
     
     /*
      // MARK: - Navigation
